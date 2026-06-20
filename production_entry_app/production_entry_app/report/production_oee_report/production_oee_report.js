288f765bc49249293f9a4ff24bca0af4bb8858da
Guru107 <guruak107@gmail.com>
2026-06-20 09:46:43 +0000

    refactor: share report date filters

--- production_entry_app/production_entry_app/report/production_oee_report/production_oee_report.js
+++ production_entry_app/production_entry_app/report/production_oee_report/production_oee_report.js
@@ -1,29 +1,6 @@
 frappe.query_reports["Production OEE Report"] = {
 	filters: [
-		{
-			fieldname: "from_date",
-			label: __("From Date"),
-			fieldtype: "Date",
-			reqd: 1,
-			default: frappe.datetime.month_start(),
-			on_change(report) {
-				window.production_entry_app?.report_filter_utils?.validate_report_date_range?.(
-					report
-				);
-			},
-		},
-		{
-			fieldname: "to_date",
-			label: __("To Date"),
-			fieldtype: "Date",
-			reqd: 1,
-			default: frappe.datetime.month_end(),
-			on_change(report) {
-				window.production_entry_app?.report_filter_utils?.validate_report_date_range?.(
-					report
-				);
-			},
-		},
+		...window.production_entry_app.report_filter_utils.get_standard_report_date_filters(),
 		{
 			fieldname: "custom_pea_workstation",
 			label: __("Workstation"),
