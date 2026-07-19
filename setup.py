e4b160397ce57d7f9996708e9cf124f22be0f23d
Guru107 <guruak107@gmail.com>
2026-07-19 11:23:47 +0000

    chore: release v1.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
 	name="production_entry_app",
-	version="0.0.1",
+	version="1.0.0",
 	description="An erpnext module to simplify production entries",
 	author="Gurudatt Kulkarni",
 	author_email="connect@gurudatt.in",
